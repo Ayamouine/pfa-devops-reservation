556e6c02fd330475ae8866ff984bedbc66364f1f
Ayamouine <ayamouine1@gmail.com>
2026-09-22 13:01:47 +0100

    chore: nettoyage du projet (suppression fichiers debug, vidéos, orphelins)

--- scripts/_dump_sidebar_disk.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const f = 'C:/Users/info/Documents/pfa_complet/pfa_complet_final/frontend/src/components/Sidebar.js';
-let s;
-try { s = fs.readFileSync(f, 'utf8'); } catch (e) { console.log('ABS'); process.exit(0); }
-const L = s.split('\n');
-console.log('TOTAL=' + L.length);
-L.forEach((l, i) => {
-  console.log(String(i + 1).padStart(3) + '|' + l);
-});
